f7dcffc33ecb3de9f2258cdfd01363a8f68aef41
Hassan Sultan <hassansultan902@gmail.com>
2021-08-07 02:45:03 +0500

    Created The App

---
+++ expense_calculator/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      title: 'Expense Calculator',
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Calculator'),
+      ),
+      body: Center(child: Text('My Widget')),
+    );
+  }
+}
